ba3b61af4acc6ca5297480c97213de4136f058ac
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-21 18:47:38 -0300

    Funcoes - declaracao, parametro e retorno

---
+++ aula-07.dart
@@ -0,0 +1,29 @@
+void main() {
+
+  printIntro();
+  printIntro();
+
+  calcSoma(10.0, 15.0);
+  double resMult = calcMult(10.0, 15.0);
+
+  print(resMult);
+
+  print(calcAreaCirculo(5.0));
+}
+
+void printIntro() {
+  print("Seja bem-vindo(a)!");
+  print("Escolha a opção!");
+}
+
+void calcSoma(double a, double b) {
+  double res = a + b;
+  print(res);
+}
+
+double calcMult(double a, double b) {
+  double res = a * b;
+  return res;
+}
+
+double calcAreaCirculo(double raio) => 3.14 * raio * raio;
