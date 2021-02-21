881a3d363b36747d561235563e7cc0f8a0b304a2
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-21 00:54:24 -0300

    Decisoes limitadas (Switch)

---
+++ aula-05.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  String linguagem = "Dart";
+
+  switch(linguagem) {
+    case "Dart":
+      print("Dart!!!");
+      break;
+    case "Java":
+      print("Java");
+      break;
+    case "Swift":
+      print("Swift");
+      break;
+    default: 
+      print("Outra...");
+  }
+}
