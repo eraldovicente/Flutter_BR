62caffcf83fcd69aafbc75f9e7ce729de254e711
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-20 23:24:25 -0200

    Declaracao e modificacao de variaveis

---
+++ aula-02.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  // Implementando uma lojinha
+
+  String nome = "Lojinha do Dart";
+  int bananas;
+  double preco = 10.50;
+  bool aindatem = true; // true - verdadeiro / false - falso
+
+  bananas = 10;
+
+  print("O nome da minha lojinha é: " + nome);
+  print("A $nome tem $bananas bananas");
+
+  dynamic teste = 1;
+  print(teste); 
+  teste = "hahaha";
+  print(teste);
+
+
+  // var teste = 1;
+  // print(teste); 
+  // teste = "hahaha";
+  // print(teste);
+
+}
