b52d11081dcfaf8de5e4fd6c0007ce1ea300c0eb
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-22 19:26:37 -0300

    Image, padding, stack

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -2,41 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [ 
-        Text("Pessoas: 0", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, decoration: TextDecoration.none, fontSize: 40.0)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              onPressed: () {}, 
-              child: Text(
-                "+1", 
-                style: TextStyle(
-                  fontSize: 40.0, 
-                  color: Colors.white)
-              )),
-            FlatButton(
-              onPressed: () {}, 
-              child: Text(
-                "-1", 
-                style: TextStyle(
-                  fontSize: 40.0, 
-                  color: Colors.white)
-              )),
-          ],
-        ),
-        Text("Pode entrar!", 
-          style: TextStyle(
-            color: Colors.white, 
-            fontWeight: FontWeight.bold, 
-            decoration: TextDecoration.none,
-            fontStyle: FontStyle.italic,
-            fontSize: 30.0
-          )),
-      ],
-    )
-  ));
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: [
+          Image.asset("images/restaurant.jpg",
+              fit: BoxFit.cover, height: 1000.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Pessoas: 0",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                      fontSize: 40.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text("+1",
+                            style: TextStyle(
+                                fontSize: 40.0, color: Colors.white))),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                        onPressed: () {},
+                        child: Text("-1",
+                            style: TextStyle(
+                                fontSize: 40.0, color: Colors.white))),
+                  ),
+                ],
+              ),
+              Text("Pode entrar!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30.0)),
+            ],
+          )
+        ],
+      )));
 }
