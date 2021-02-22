bcdb5bc2922cb7c8cc1ab00922f2f6400532edda
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-22 15:08:49 -0300

    Modificadores - static, final e const

---
+++ aula-14.dart
@@ -0,0 +1,17 @@
+class Valores {
+
+  static int vezesClicado;
+}
+
+class Pessoa {
+
+}
+
+void main() {
+
+  Valores.vezesClicado = 2;
+
+  const numero = 3;
+
+  final Pessoa pessoa = Pessoa();
+}
