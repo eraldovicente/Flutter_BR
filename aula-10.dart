8b5dbbcc8752b9bd065cef97ba9d110a7abf9bd8
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-22 01:23:57 -0300

    Construtores

--- aula-10.dart
+++ aula-10.dart
@@ -4,6 +4,20 @@ class Pessoa {
   int idade;
   double altura;
 
+  // Pessoa(String nome, int idade, double altura) {
+  //   this.nome = nome;
+  //   this.idade = idade;
+  //   this.altura = altura;
+  // }
+
+  Pessoa(this.nome, this.idade, this.altura);
+
+  Pessoa.nascer(this.nome, this.altura) {
+    idade = 0;
+    print("$nome nasceu!");
+    dormir();
+  }
+
   void dormir() {
     print("$nome está dormindo!");
   }
@@ -17,15 +31,19 @@ void main() {
 
   String algumaCoisa = "valor";
 
-  Pessoa pessoa1 = new Pessoa();
-  pessoa1.nome = "Eraldo";
-  pessoa1.idade = 30;
-  pessoa1.altura = 1.73;
+  // Pessoa pessoa1 = new Pessoa();
+  // pessoa1.nome = "Eraldo";
+  // pessoa1.idade = 30;
+  // pessoa1.altura = 1.73;
 
-  Pessoa pessoa2 = Pessoa();
-  pessoa2.nome = "Gabriel";
-  pessoa2.idade = 6;
-  pessoa2.altura = 1.20;
+  // Pessoa pessoa2 = Pessoa();
+  // pessoa2.nome = "Gabriel";
+  // pessoa2.idade = 6;
+  // pessoa2.altura = 1.20;
+
+  Pessoa pessoa1 = new Pessoa("Eraldo", 30, 1.73);
+
+  Pessoa pessoa2 = Pessoa("Gabriel", 6, 1.20);
 
   print(pessoa1.nome);
   print(pessoa2.nome);
@@ -35,4 +53,8 @@ void main() {
   print(pessoa1.idade);
 
   pessoa2.dormir();
+
+  Pessoa nene = Pessoa.nascer("Enzo", 0.30);
+  print(nene.nome);
+  print(nene.idade);
 }
