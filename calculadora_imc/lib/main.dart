f3c9ba6c1bb857d5e6ef811f7c295bf34e85776b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-22 23:07:12 -0300

    TextField e decoration

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -15,16 +13,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text("Calculadora IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            )
+          ],
+        ));
   }
 }
