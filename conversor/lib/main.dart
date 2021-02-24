d0b3ffe7eecb44ef1535dbda22369f5bf5f0e53e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-23 22:09:36 -0300

    Otimizando o codigo

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -35,8 +35,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double dolar;
   double euro;
+
+  void _realChanged(String text) {
+    print(text);
+  }
+
+  void _dolarChanged(String text) {
+    print(text);
+  }
+
+  void _euroChanged(String text) {
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,52 +85,36 @@ class _HomeState extends State<Home> {
                   euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
                   return SingleChildScrollView(
-                  padding: EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Icon(Icons.monetization_on,
-                          size: 150.0, color: Colors.amber),
-                      TextField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).hintColor)),
-                            labelText: "Reais",
-                            labelStyle: TextStyle(color: Colors.amber),
-                            border: OutlineInputBorder(),
-                            prefixText: "R\$ "),
-                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                      ),
-                      Divider(),
-                      TextField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).hintColor)),
-                            labelText: "Dolares",
-                            labelStyle: TextStyle(color: Colors.amber),
-                            border: OutlineInputBorder(),
-                            prefixText: "US\$ "),
-                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                      ),
-                      Divider(),
-                      TextField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Theme.of(context).hintColor)),
-                            labelText: "Euros",
-                            labelStyle: TextStyle(color: Colors.amber),
-                            border: OutlineInputBorder(),
-                            prefixText: "€\$ "),
-                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                      ),
-                    ],
-                  ));
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber),
+                          buildTextField("Reais", "R\$ ", realController, _realChanged),
+                          Divider(),
+                          buildTextField("Dolares", "US\$ ", dolarController, _dolarChanged),
+                          Divider(),
+                          buildTextField("Euros", "€\$ ", euroController, _euroChanged),
+                        ],
+                      ));
                 }
             }
           }),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController c, Function f) {
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    onChanged: f,
+    keyboardType: TextInputType.number,
+  );
 }
