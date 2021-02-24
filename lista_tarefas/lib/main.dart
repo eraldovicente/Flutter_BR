6699c4ea9eb551550b5ee56832008951d5d6e271
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-24 14:06:16 -0300

    Criando a lista

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -14,7 +14,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = [];
+  List _toDoList = ["Eraldo", "Vicente"];
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +45,22 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index) {
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index]["title"]),
+                    value: _toDoList[index]['ok'],
+                    secondary: CircleAvatar(
+                        child: Icon(_toDoList[index]["ok"]
+                            ? Icons.check
+                            : Icons.error)),
+                    onChanged: (bool value) {},
+                  );
+                }),
           )
         ],
       ),
