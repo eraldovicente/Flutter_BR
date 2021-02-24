43880a61e94d549642993292b913b0c30b6cdd40
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-24 16:33:39 -0300

    Ordenando os itens

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -43,6 +43,19 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a,b) {
+        if (a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,10 +88,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
-                itemBuilder: buildItem),
+                itemBuilder: buildItem) 
+              )
           )
         ],
       ),
