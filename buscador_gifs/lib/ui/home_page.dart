36d8cbf9a3c364fdad8e25c16342a73ffd705952
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-25 13:14:08 -0300

    Iniciando o layout

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int offset = 0;
@@ -19,9 +18,11 @@ class _HomePageState extends State<HomePage> {
     http.Response response;
 
     if (_search == null)
-      response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=KkBI75MjiBdAMP2dSObGV0fZSo3MB27A&limit=20&rating=g");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=KkBI75MjiBdAMP2dSObGV0fZSo3MB27A&limit=20&rating=g");
     else
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=KkBI75MjiBdAMP2dSObGV0fZSo3MB27A&q=$_search&limit=20&offset=$offset&rating=g&lang=en");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=KkBI75MjiBdAMP2dSObGV0fZSo3MB27A&q=$_search&limit=20&offset=$offset&rating=g&lang=en");
 
     return json.decode(response.body);
   }
@@ -35,10 +36,31 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Pesquise aqui",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
