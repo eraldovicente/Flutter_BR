46f33f471cdc0e21cfa52cc3aac89e0989f6c10f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-02-25 22:06:22 -0300

    Imagens suaves

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 import 'gif_page.dart';
 
@@ -116,10 +117,12 @@ class _HomePageState extends State<HomePage> {
           if(_search == null || index < snapshot.data['data'].length) {
 
             return GestureDetector(
-              child: Image.network(
-                  snapshot.data['data'][index]['images']['fixed_height']['url'],
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage, 
+                image: snapshot.data['data'][index]['images']['fixed_height']['url'],
                   height: 300.0,
-                  fit: BoxFit.cover),
+                  fit: BoxFit.cover,
+                ),
                   onTap: () {
                     Navigator.push(context,
                       MaterialPageRoute(builder: (context) => GifPage(snapshot.data['data'][index]))
