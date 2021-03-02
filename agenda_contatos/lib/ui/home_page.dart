9b3e374bff3f01d8dcd8d77212bbb543e5a1847d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-01 23:41:49 -0300

    Ordenando os contatos

--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 
+enum OrderOptions {orderaz, orderza}
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -36,6 +38,21 @@ class _HomePageState extends State<HomePage> {
           title: Text("Contatos"),
           backgroundColor: Colors.red,
           centerTitle: true,
+          actions: [
+            PopupMenuButton<OrderOptions>(
+              itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+                const PopupMenuItem<OrderOptions>(
+                  child: Text("Ordenar de A-Z"),
+                  value: OrderOptions.orderaz
+                ),
+                const PopupMenuItem<OrderOptions>(
+                  child: Text("Ordenar de Z-A"),
+                  value: OrderOptions.orderza
+                )
+              ], 
+              onSelected: _orderList, 
+            )
+          ],
         ),
         backgroundColor: Colors.white,
         floatingActionButton: FloatingActionButton(
@@ -171,6 +188,24 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         contacts = list;
       });
+    });
+  }
+
+  void _orderList(OrderOptions result) {
+    switch(result) {
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+      
     });
   }
 }
