2f8acd0f4c09ebc4bae0a057994f0cb9c6e93258
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-09 20:39:14 -0300

    Estruturando as categorias

--- Loja_Virtual/lib/screens/home_screen.dart
+++ Loja_Virtual/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:Loja_Virtual/widgets/custom_drawer.dart';
 
+import 'package:Loja_Virtual/tabs/products_tab.dart';
 import 'package:Loja_Virtual/tabs/home_tab.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,7 +19,14 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.red),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
         Container(color: Colors.yellow),
         Container(color: Colors.green),
       ]
