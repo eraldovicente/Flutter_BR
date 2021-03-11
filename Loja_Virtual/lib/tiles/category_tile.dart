d913688e1f014b42222d0b62a3bc4355f9de8a15
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-11 19:55:08 -0300

    Criando tela de produtos

--- Loja_Virtual/lib/tiles/category_tile.dart
+++ Loja_Virtual/lib/tiles/category_tile.dart
@@ -1,3 +1,4 @@
+import 'package:Loja_Virtual/screens/category_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -19,7 +20,9 @@ class CategoryTile extends StatelessWidget {
       title: Text(snapshot.data["title"]),
       trailing: Icon(Icons.keyboard_arrow_right),
       onTap: () {
-
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => CategoryScreen(snapshot))
+        );
       },
     );
   }
