64d0f688359f6d6c3b5097916dd53043ff35cbc8
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-11 23:02:30 -0300

    Criando a tile de lista

--- Loja_Virtual/lib/tiles/product_tile.dart
+++ Loja_Virtual/lib/tiles/product_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:Loja_Virtual/screens/product_screen.dart';
 import 'package:Loja_Virtual/datas/product_data.dart';
 
 class ProductTile extends StatelessWidget {
@@ -10,6 +11,11 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(product))
+        );
+      },
       child: Card(
         child: type == "grid" ?
           Column(
@@ -47,7 +53,42 @@ class ProductTile extends StatelessWidget {
               )
             ],
           )
-          : Row()
+          : Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Image.network(
+                  product.images[0],
+                  fit: BoxFit.cover,
+                  height: 250.0
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(product.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                        "R\$ ${product.price.toStringAsFixed(2)}",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              )
+            ],
+          )
       ),
     );
   }
