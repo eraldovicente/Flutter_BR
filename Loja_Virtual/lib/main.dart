bb3a1ebd2c2c8476e3e3aa66f8a62179ab8906eb
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-12 20:37:33 -0300

    Introducao ao scopedModel

--- Loja_Virtual/lib/main.dart
+++ Loja_Virtual/lib/main.dart
@@ -1,24 +1,26 @@
+import 'package:Loja_Virtual/models/user_models.dart';
 import 'package:Loja_Virtual/screens/login_screen.dart';
 import 'package:Loja_Virtual/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:Loja_Virtual/screens/home_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() => runApp(new MyApp());
 
-
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter's Clothing",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: "Flutter's Clothing",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Color.fromARGB(255, 4, 125, 141)),
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
     );
   }
 }
