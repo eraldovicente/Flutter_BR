ca8dd5b8172feda93d1caaddee792b7a7fb4a94c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-17 16:38:48 -0300

    Recuperando os dados do produto

--- Loja_Virtual/lib/screens/cart_screen.dart
+++ Loja_Virtual/lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:Loja_Virtual/models/user_models.dart';
 import 'package:Loja_Virtual/screens/login_screen.dart';
+import 'package:Loja_Virtual/tiles/cart_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:Loja_Virtual/models/cart_models.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -26,49 +27,63 @@ class CartScreen extends StatelessWidget {
           )
         ],
       ),
-      body: ScopedModelDescendant<CartModel>(
-        builder: (context, child, model) {
-          if(model.isLoading && UserModel.of(context).isLoggedIn()){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (!UserModel.of(context).isLoggedIn()) {
-            return Container(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.remove_shopping_cart, size: 80.0, color: Theme.of(context).primaryColor),
-                  SizedBox(height: 16.0),
-                  Text("Faça o login para adicionar produtos!",
-                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center
-                    ),     
-                    SizedBox(height: 16.0),
-                    RaisedButton(
-                      child: Text("Entrar", style: TextStyle(fontSize: 18.0),),
-                      textColor: Colors.white,
-                      color: Theme.of(context).primaryColor,
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context)=>LoginScreen())
-                        );
-                      },
-                    )             
-                ],
-              ),
-            );
-          } else if(model.products == null || model.products.length == 0) {
-            return Center(
-              child: Text("Nenhum produto no carrinho!",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
+      body: ScopedModelDescendant<CartModel>(builder: (context, child, model) {
+        if (model.isLoading && UserModel.of(context).isLoggedIn()) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!UserModel.of(context).isLoggedIn()) {
+          return Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.remove_shopping_cart,
+                    size: 80.0, color: Theme.of(context).primaryColor),
+                SizedBox(height: 16.0),
+                Text("Faça o login para adicionar produtos!",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center),
+                SizedBox(height: 16.0),
+                RaisedButton(
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                )
+              ],
+            ),
+          );
+        } else if (model.products == null || model.products.length == 0) {
+          return Center(
+            child: Text(
+              "Nenhum produto no carrinho!",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          );
+        } else {
+          return ListView(
+            children: [
+              Column(
+                children: model.products.map(
+                  (product) {
+                    return CartTile(product);
+                  }
+                ).toList(),
+              )
+            ],
+          );
         }
-      ),
+      }),
     );
   }
 }
